a33fcf418b0edbd04a9248a543cd8438dce0bacd
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2016-06-24 19:14:54 -0300

    adicionando api para calcular o digito verificador

--- lib/sigep_web.rb
+++ lib/sigep_web.rb
@@ -21,6 +21,7 @@ require 'sigep_web/request_plp_services'
 require 'sigep_web/postage_card_status'
 require 'sigep_web/request_xml_plp'
 require 'sigep_web/request_range'
+require 'sigep_web/calculate_digit_checker'
 require 'savon'
 require 'nokogiri'
 
@@ -67,5 +68,9 @@ module SigepWeb
 
   def self.request_range(options = {})
     RequestRange.new(options).request
+  end
+
+  def self.calculate_digit_checker(options = {})
+    CalculateDigitChecker.new(options).request
   end
 end
