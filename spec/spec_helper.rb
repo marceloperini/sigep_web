b2a31a8626f5e0528b5d9882bb16991b217c767c
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2017-11-18 15:18:10 -0200

    add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sigep_web'
+require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+    add_filter 'spec/'
+end
 
 ENV['GEM_ENV'] = 'test'
