80d7665bb0adea70da7355ae4afa6bed5f4a5bd5
Marcelo Perini Veloso <marcelo.perini.veloso@gmail.com>
2020-04-17 21:52:01 -0300

    improve SigepWeb spec

--- spec/sigep_web_spec.rb
+++ spec/sigep_web_spec.rb
@@ -66,4 +66,82 @@ RSpec.describe SigepWeb do
       expect(search_client).to have_received(:request)
     end
   end
+
+  describe '.zip_query' do
+    let(:zip_query) { instance_double(SigepWeb::ZipQuery) }
+
+    before do
+      allow(SigepWeb::ZipQuery).to receive(:new).and_return(zip_query)
+      allow(zip_query).to receive(:request)
+
+      sigep_web.zip_query
+    end
+
+    it 'is expeceted to access SigepWeb::ZipQuery' do
+      expect(zip_query).to have_received(:request)
+    end
+  end
+
+  describe '.request_labels' do
+    let(:request_labels) { instance_double(SigepWeb::RequestLabels) }
+
+    before do
+      allow(SigepWeb::RequestLabels).to receive(:new).and_return(request_labels)
+      allow(request_labels).to receive(:request)
+
+      sigep_web.request_labels
+    end
+
+    it 'is expeceted to access SigepWeb::RequestLabels' do
+      expect(request_labels).to have_received(:request)
+    end
+  end
+
+  describe '.generate_labels_digit_verifier' do
+    let(:generate_labels_digit_verifier) { instance_double(SigepWeb::GenerateLabelsDigitVerifier) }
+
+    before do
+      allow(SigepWeb::GenerateLabelsDigitVerifier)
+        .to receive(:new).and_return(generate_labels_digit_verifier)
+      allow(generate_labels_digit_verifier).to receive(:request)
+
+      sigep_web.generate_labels_digit_verifier
+    end
+
+    it 'is expeceted to access SigepWeb::GenerateLabelsDigitVerifier' do
+      expect(generate_labels_digit_verifier).to have_received(:request)
+    end
+  end
+
+  describe '.request_ptp_services' do
+    let(:request_ptp_services) { instance_double(SigepWeb::RequestPlpServices) }
+
+    before do
+      allow(SigepWeb::RequestPlpServices)
+        .to receive(:new).and_return(request_ptp_services)
+      allow(request_ptp_services).to receive(:request)
+
+      sigep_web.request_plp_services
+    end
+
+    it 'is expeceted to access SigepWeb::RequestPlpServices' do
+      expect(request_ptp_services).to have_received(:request)
+    end
+  end
+
+  describe '.postage_card_status' do
+    let(:postage_card_status) { instance_double(SigepWeb::PostageCardStatus) }
+
+    before do
+      allow(SigepWeb::PostageCardStatus)
+        .to receive(:new).and_return(postage_card_status)
+      allow(postage_card_status).to receive(:request)
+
+      sigep_web.postage_card_status
+    end
+
+    it 'is expeceted to access SigepWeb::PostageCardStatus' do
+      expect(postage_card_status).to have_received(:request)
+    end
+  end
 end
